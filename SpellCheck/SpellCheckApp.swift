2cac8ab6d9e4d3232dc899508bb89f0f9a1271a1
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-03-09 15:01:54 +0300

    Initial Commit

---
+++ SpellCheck/SpellCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellCheckApp.swift
+//  SpellCheck
+//
+//  Created by Vasichko Anna on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpellCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
