3c81e445aa0894d4d966bc827253ae06f6102e34
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-03-09 16:11:16 +0300

    textChecker

--- SpellCheck/ContentView.swift
+++ SpellCheck/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+    @State private var suggestions: [String]? = []
+    
+    private let textChecker = UITextChecker()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Text("Enter some word:")
+                    .font(.title)
+                TextField("", text: $text)
+                    .textFieldStyle(.roundedBorder)
+                    .overlay(RoundedRectangle(cornerRadius: 15) .stroke(Color.blue))
+                    .frame(width: 250)
+            }
+            .padding()
+            
+            if let suggestions {
+                List(suggestions, id: \.self) { suggestion in
+                    HStack {
+                        Text(suggestion)
+                        Spacer()
+                    }
+                    .onTapGesture {
+                        var components = text.components(separatedBy: " ")
+                        components.removeLast()
+                        components.append(suggestion)
+                        text = components.joined(separator: " ")
+                    }
+                }
+            }
+     
         }
+        .onChange(of: text, perform: { newValue in
+            let misspelledRange = textChecker.rangeOfMisspelledWord(
+                in: text,
+                range: NSRange(0..<text.utf16.count),
+                startingAt: 0,
+                wrap: false,
+                language: "en_US"
+            )
+            if misspelledRange.location != NSNotFound {
+                suggestions = textChecker.guesses(
+                    forWordRange: misspelledRange,
+                    in: text,
+                    language: "en_US"
+                )
+            }
+        }
+        )
         .padding()
     }
 }
